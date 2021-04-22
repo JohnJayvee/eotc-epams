379433a1d39270e7d10095c6fb65de384a126019
maganasamuel <maganasamuel001@gmail.com>
2021-04-22 17:40:06 +1200

    Created signup sidebar shared component

---
+++ app/Laravel/Views/web/_components/sidebar-signup.blade.php
@@ -0,0 +1,40 @@
+<div class="tw-flex tw-flex-shrink-0">
+  <div class="tw-flex tw-flex-col">
+    <div class="tw-flex tw-flex-col tw-h-0 tw-flex-1">
+      <div
+        class="guest-sidebar tw-flex-1 tw-flex tw-flex-col tw-overflow-y-auto tw-bg-blue tw-text-white tw-py-20 tw-px-16">
+        <a href="{{ route('web.login') }}"
+          class="tw-flex tw-items-center tw-justify-start tw-text-white hover:tw-text-white tw-space-x-6">
+          <i class="fas fa-chevron-left tw-w-4 tw-text-3xl"></i>
+          <div class="tw-flex-shrink-1 tw-font-bold tw-text-2xl">Back to<br>Sign
+            In</div>
+        </a>
+        <img src="/web/img/signup-logo.svg" class="tw-mx-auto tw-mt-40" />
+        <div class="tw-mx-auto tw-mt-12">
+          <div class="tw-flex tw-items-center tw-justify-center tw-space-x-4">
+            <span class="tw-text-base">1</span>
+            <span class="tw-text-2xl">Account Information</span>
+          </div>
+          <div class="tw-text-2xl text-center tw-opacity-30">|</div>
+          <div
+            class="tw-flex tw-items-center tw-justify-center tw-space-x-4 tw-opacity-30">
+            <span class="tw-text-base">2</span>
+            <span class="tw-text-2xl">Contact Information</span>
+          </div>
+          <div class="tw-text-2xl text-center tw-opacity-30">|</div>
+          <div
+            class="tw-flex tw-items-center tw-justify-center tw-space-x-4 tw-opacity-30">
+            <span class="tw-text-base">3</span>
+            <span class="tw-text-2xl">Company Profile</span>
+          </div>
+          <div class="tw-text-2xl text-center tw-opacity-30">|</div>
+          <div
+            class="tw-flex tw-items-center tw-justify-center tw-space-x-4 tw-opacity-30">
+            <span class="tw-text-base">4</span>
+            <span class="tw-text-2xl">Verification</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
